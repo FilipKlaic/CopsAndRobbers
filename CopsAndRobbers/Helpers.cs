e08c58518e4bc9ed0dc32e0f2f4c045a08334f1d
Tr1xter0 <115631353+FilipKlaic@users.noreply.github.com>
2025-10-21 15:19:49 +0200

    Project commit

---
+++ CopsAndRobbers/Helpers.cs
@@ -0,0 +1,51 @@
+﻿namespace CopsAndRobbers
+{
+    internal class Helpers
+    {
+
+        public static string[,] DrawingClass()
+        {
+            string[,] drawingCreated = new string[10, 15];
+
+            for (int row = 0; row < drawingCreated.GetLength(0); row++)
+            {
+                for (int col = 0; col < drawingCreated.GetLength(1); col++)
+                {
+                    if (row == 0 || row == drawingCreated.GetLength(0) - 1 || //top,mid,bot
+                        col == 0 || col == drawingCreated.GetLength(1) - 1)
+                    {
+                        drawingCreated[row, col] = "#";
+
+                    }
+                    else
+                    {
+                        drawingCreated[row, col] = " ";
+                    }
+
+                    Console.Write(drawingCreated[row, col]);
+                }
+                Console.WriteLine();
+
+            }
+            return drawingCreated;
+        }
+
+        public static string[,] UpdateDrawing(string[,] drawingImport)  // den uppdaterade teckningen
+        {
+            Console.Clear();
+
+            for (int row = 0; row < drawingImport.GetLength(0); row++)
+            {
+                for (int col = 0; col < drawingImport.GetLength(1); col++)  //rita upp allt
+                {
+                    Console.Write(drawingImport[row, col]); //rita ut den uppdaterade rad för rad , med den nya bokstaven
+
+                }
+                Console.WriteLine();
+
+            }
+
+            return drawingImport; //skicka tillbaka färdig ritning
+        }
+    }
+}
---
+++ CopsAndRobbers/Person.cs
@@ -0,0 +1,71 @@
+﻿namespace CopsAndRobbers
+{
+    internal class Person : Inventory
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Character { get; set; }
+        public ConsoleColor Charactercolor { get; set; }
+
+        public Person() { }
+
+        public Person(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public virtual void Draw()
+        {
+            Console.ForegroundColor = Charactercolor;
+            Console.SetCursorPosition(Y, X);
+            Console.Write(Character);
+            Console.ResetColor();
+        }
+    }
+
+    class Thief : Person
+    {
+        public Thief(int randomX, int randomY) : base(randomX, randomY)
+        {
+            Character = "T";
+            Charactercolor = ConsoleColor.Red;
+        }
+
+        public Thief()
+        {
+            Character = "T";
+            Charactercolor = ConsoleColor.Red;
+        }
+    }
+
+    class Police : Person
+    {
+        public Police(int randomX, int randomY) : base(randomX, randomY)
+        {
+            Character = "P";
+            Charactercolor = ConsoleColor.Blue;
+        }
+
+        public Police()
+        {
+            Character = "P";
+            Charactercolor = ConsoleColor.Blue;
+        }
+    }
+
+    class Civilian : Person
+    {
+        public Civilian(int randomX, int randomY) : base(randomX, randomY)
+        {
+            Character = "C";
+            Charactercolor = ConsoleColor.Green;
+        }
+
+        public Civilian()
+        {
+            Character = "C";
+            Charactercolor = ConsoleColor.Green;
+        }
+    }
+}
