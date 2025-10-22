b179d942059df2a9d385a9a2ea484dbb865a71bd
YevShch <yevheniiashcherbakova82@gmail.com>
2025-10-22 08:24:11 +0200

    added classes Place, Prison and City

---
+++ CopsAndRobbers/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsAndRobbers
+{
+    class City : Place
+    {
+        public City() : base("CITY", 100, 25)
+        {
+        }
+
+        // City's methods can be added
+    }
+}
