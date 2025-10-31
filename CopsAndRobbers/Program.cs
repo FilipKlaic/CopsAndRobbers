ed5724fc94bae62e138a6141e2d596e08038fa84
Tr1xter0 <115631353+FilipKlaic@users.noreply.github.com>
2025-10-31 11:17:56 +0100

    fallback method

--- CopsAndRobbers/Program.cs
+++ CopsAndRobbers/Program.cs
@@ -1,43 +1,58 @@
-﻿namespace CopsAndRobbers
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace CopsAndRobbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-   
             Random rnd = new Random();
-
+            try
+            {
+                Console.SetWindowSize(120, 50);  // width, height
+                Console.SetBufferSize(120, 50);
+            }
+            catch (Exception)
+            {
+                // If we can't resize, continue with current size
+                Console.WriteLine("Could not resize console window. Game may not display properly.");
+                Console.WriteLine("Please maximize your console window.");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
             // Create characters without coordinates
             List<Person> characters = new List<Person>
-    {
-        new Civilian(0,0,"Jonsson"),
-        new Civilian(0,0,"Svensson"),
-        new Civilian(0,0,"Olofsson"),
-        new Thief(0,0,"Silverstedt"),
-        new Thief(0,0,"Brorson"),
-        new Thief(0,0,"Olsson"),
-        new Police(0,0,"Johansson"),
-        new Police(0,0,"Karlsson"),
-        new Police(0,0,"Nilsson"),
-         new Civilian(0,0,"Jonsson2"),
-        new Civilian(0,0,"Svensson2"),
-        new Civilian(0,0,"Olofsson2"),
-        new Thief(0,0,"Silverstedt2"),
-        new Thief(0,0,"Brorson2"),
-        new Thief(0,0,"Olsson2"),
-        new Police(0,0,"Johansson2"),
-        new Police(0,0,"Karlsson2"),
-        new Police(0,0,"Nilsson2"),
-        new Civilian(0,0,"Jonsson3"),
-        new Civilian(0,0,"Svensson3"),
-        new Civilian(0,0,"Olofsson3"),
-        new Thief(0,0,"Silverstedt3"),
-        new Thief(0,0,"Brorson3"),
-        new Thief(0,0,"Olsson3"),
-        new Police(0,0,"Johansson3"),
-        new Police(0,0,"Karlsson3"),
-        new Police(0,0,"Nilsson3")
-    };
+            {
+                new Civilian(0,0,"Jonsson"),
+                new Civilian(0,0,"Svensson"),
+                new Civilian(0,0,"Olofsson"),
+                new Thief(0,0,"Silverstedt"),
+                new Thief(0,0,"Brorson"),
+                new Thief(0,0,"Olsson"),
+                new Police(0,0,"Johansson"),
+                new Police(0,0,"Karlsson"),
+                new Police(0,0,"Nilsson"),
+                new Civilian(0,0,"Jonsson2"),
+                new Civilian(0,0,"Svensson2"),
+                new Civilian(0,0,"Olofsson2"),
+                new Thief(0,0,"Silverstedt2"),
+                new Thief(0,0,"Brorson2"),
+                new Thief(0,0,"Olsson2"),
+                new Police(0,0,"Johansson2"),
+                new Police(0,0,"Karlsson2"),
+                new Police(0,0,"Nilsson2"),
+                new Civilian(0,0,"Jonsson3"),
+                new Civilian(0,0,"Svensson3"),
+                new Civilian(0,0,"Olofsson3"),
+                new Thief(0,0,"Silverstedt3"),
+                new Thief(0,0,"Brorson3"),
+                new Thief(0,0,"Olsson3"),
+                new Police(0,0,"Johansson3"),
+                new Police(0,0,"Karlsson3"),
+                new Police(0,0,"Nilsson3")
+            };
 
             // Create two new objects: City and Prison
             City city = new City();
