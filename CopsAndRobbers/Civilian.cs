77eb608ade837011fd87b99c1b956314e375b3f5
YevShch <yevheniiashcherbakova82@gmail.com>
2025-10-31 09:42:35 +0100

    refactoring

---
+++ CopsAndRobbers/Civilian.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsAndRobbers
+{
+    internal class Civilian : Person
+    {
+        public Civilian(int randomX, int randomY, string name = "Unknown Civilian") : base(randomX, randomY, name, "Civilian")
+        {
+            Character = "C";
+            Charactercolor = ConsoleColor.Green;
+            Inventory = new Inventory(new List<string> { "Keys", "Phone", "Cash", "Watch" });
+        }
+
+    }
+}
---
+++ CopsAndRobbers/Police.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsAndRobbers
+{
+    class Police : Person
+    {
+        public Police(int randomX, int randomY, string name = "Unknown Police officer") : base(randomX, randomY, name, "Police officer")
+        {
+            Character = "P";
+            Charactercolor = ConsoleColor.Blue;
+        }
+
+
+    }
+
+}
